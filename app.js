d26185d315fe9ea93afe8572fe5a94030907f049
Nolan DE PUYDT <nodepuydt@gmail.com>
2023-06-12 11:48:27 +0200

    first commit

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+const { hashPassword } = require("./auth.js");
+
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
@@ -26,8 +28,8 @@ const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
-app.put("/api/users/:id", userHandlers.updateUser);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
